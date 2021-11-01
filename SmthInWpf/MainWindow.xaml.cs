81b44585e335e36f1fefd18a1d02f2890a561943
0leg-zharkov <60039706+0leg-zharkov@users.noreply.github.com>
2021-11-01 17:09:27 +0300

    Добавьте файлы проекта.

---
+++ SmthInWpf/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmthInWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+        public Player player { get; set; }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            L1.Content = TX1.Text;
+            L2.Content = TX2.Text;
+            L3.Content = TX3.Text;
+        }
+    }
+}
---
+++ SmthInWpf/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace SmthInWpf
+{
+    public class Player : INotifyPropertyChanged
+    {
+        private string name;
+        private string type;
+        private int level;
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string nameProp)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameProp));
+        }
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name == value) return;
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+
+        public string Type
+        {
+            get => type;
+            set
+            {
+                if (type == value) return;
+                type = value;
+                OnPropertyChanged("Type");
+            }
+        }
+
+
+    }
+}
